62770f04303986dca6a4da94c4501ac78d20551a
Luppole <itamarperetzofficial@gmail.com>
2024-02-24 01:13:28 +0200

    The original code

---
+++ functions.py
@@ -0,0 +1,135 @@
+import base64
+import urllib.parse
+import re
+import requests
+import jwt
+
+def jwtdec(jwtstr, key):
+    encoded_token = 'your_encoded_jwt_token'
+    secret_key = 'your_secret_key'
+
+    try:
+        decoded_token = jwt.decode(encoded_token, secret_key, algorithms=['HS256'])
+        return f"Decoded Token: {decoded_token}"
+    except jwt.ExpiredSignatureError:
+        return "Token has expired"
+    except jwt.InvalidTokenError:
+        return "Invalid token"
+
+def b64enc(text):
+    try:
+        encoded_text = f"**base64.b64encode(text.encode('utf-8')).decode('utf-8')**"
+    except:
+        encoded_text = "**Invalid Input**"
+
+    return encoded_text
+
+
+def b64dec(text):
+    try:
+        decoded_text = f"**base64.b64decode(text.encode('utf-8')).decode('utf-8')**"
+    except:
+        decoded_text = "**Invalid Input**"
+
+    return decoded_text
+
+
+def b32enc(text):
+    try:
+        encoded_text = base64.b32encode(text.encode('utf-8')).decode('utf-8')
+    except:
+        encoded_text = "**Invalid Input**"
+
+    return encoded_text
+
+
+def b32dec(text):
+    try:
+        decoded_text = base64.b32decode(text.encode('utf-8')).decode('utf-8')
+    except:
+        decoded_text = "**Invalid Input**"
+    return decoded_text
+
+
+def bin_to_hex(binary_str):
+    try:
+        return hex(int(binary_str, 2))[2:]
+    except:
+        return "**Invalid Input**"
+
+def hex_to_bin(hex_str):
+    if not all(c in '0123456789ABCDEFabcdef' for c in hex_str):
+        raise ValueError('Invalid hexadecimal string')
+    return bin(int(hex_str, 16))[2:]
+
+
+def hex_to_dec(hex_str):
+    return int(hex_str, 16)
+
+
+def dec_to_hex(dec_str):
+    return hex(int(dec_str)).split('x')[-1].upper()
+
+
+def bin_to_str(bins):
+    return ''.join(chr(int(bins[i:i+8], 2)) for i in range(0, len(bins), 8))
+
+
+def str_to_bin(string):
+    return ''.join(bin(ord(c))[2:].zfill(8) for c in string)
+
+
+def cyclic(num):
+    text = ""
+    alphabeta = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+                 'u', 'v', 'w', 'x', 'y', 'z']
+    buffer = "aaa"
+    counter = 0
+    for i in range(num):
+        if int(i / 26) > counter:
+            counter += 1
+            buffer = buffer.replace(buffer[-(len(buffer) - (int(counter / 26)))],
+                                    chr(ord(buffer[-(int(counter / 26))]) + 1))
+        text += alphabeta[i % 26]
+        text += buffer
+    return text
+
+
+def locate_cyclic(cyc, subs):
+    index = cyc.find(subs)
+    if index == -1:
+        return "Not found"
+    return index
+
+
+def encode_url(url):
+    return urllib.parse.quote(url)
+
+
+def decode_url(url):
+    return re.compile('%([0-9a-fA-F]{2})', re.M).sub(lambda m: chr(int(m.group(1), 16)), url)
+
+def get_ctf_events():
+    url = 'https://ctftime.org/api/v1/events/'
+    params = {'limit': 100}
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (HTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    response = requests.get(url, params=params, headers=headers)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f'Error: {response.status_code}')
+        return None
+
+
+def team_info():
+    url = 'https://ctftime.org/api/v1/teams/266864/'
+    params = {'limit': 100}
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (HTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    response = requests.get(url, params=params, headers=headers)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f'Error: {response.status_code}')
+        return None
---
+++ main.py
@@ -0,0 +1,173 @@
+import discord
+from discord.ext import commands
+import base64
+import urllib.parse
+import re
+import requests
+from datetime import datetime
+import functions
+
+intents = discord.Intents.default()
+intents.message_content = True
+help_command = commands.DefaultHelpCommand(no_category='Commands')
+bot = commands.Bot(command_prefix='!', intents=intents, help_command=None)
+
+
+@bot.command(name='64enc', description="Encoding a text using Base64", category="Encoding", help_command=help_command)
+async def encode64(ctx, *, text):
+    encoded_text = functions.b64enc(text)
+    await ctx.send(f"Original Text: {text}\nBase64 Encoded: {encoded_text}")
+
+
+@bot.command(name='64dec')
+async def decode64(ctx, *, text):
+    encoded_text = functions.b64dec(text)
+    await ctx.send(f"Original Text: {text}\nBase64 Decoded: {encoded_text}")
+
+
+@bot.command(name='32enc')
+async def encode64(ctx, *, text):
+    encoded_text = functions.b32enc(text)
+    await ctx.send(f"Original Text: {text}\nBase32 Encoded: {encoded_text}")
+
+
+@bot.command(name='32dec')
+async def decode64(ctx, *, text):
+    encoded_text = functions.b32dec(text)
+    await ctx.send(f"Original Text: {text}\nBase32 Decoded: {encoded_text}")
+
+
+@bot.command(name='cyc')
+async def cyclic_gen(ctx, *, num):
+    text = functions.cyclic(int(num))
+    await ctx.send(text)
+
+
+@bot.command(name='cycfind')
+async def cyclic_find(ctx, string: str, substring: str):
+    text = functions.locate_cyclic(string, substring)
+    await ctx.send(text)
+
+
+@bot.command(name='urlenc')
+async def url_enc(ctx, *, url):
+    text = functions.encode_url(url)
+    await ctx.send(text)
+
+
+@bot.command(name='urldec')
+async def url_dec(ctx, *, url):
+    text = functions.decode_url(url)
+    await ctx.send(text)
+
+
+@bot.command(name='hextobin')
+async def hex_to_bin(ctx, *, text):
+    encoded_text = functions.hex_to_bin(text)
+    await ctx.send(f"Original Text: {text}\nBinary Value: {encoded_text}")
+
+
+@bot.command(name='bintohex')
+async def bin_to_hex(ctx, *, text):
+    encoded_text = functions.bin_to_hex(text)
+    await ctx.send(f"Original Text: {text}\nHexadecimal Value: {encoded_text}")
+
+
+@bot.command(name='hextodec')
+async def hex_to_dec(ctx, *, text):
+    encoded_text = functions.hex_to_dec(text)
+    await ctx.send(f"Original Text: {text}\nDecimal Value: {encoded_text}")
+
+
+@bot.command(name='dectohex')
+async def dec_to_hex(ctx, *, text):
+    encoded_text = functions.dec_to_hex(text)
+    await ctx.send(f"Original Text: {text}\nHexadecimal Value: {encoded_text}")
+
+
+@bot.command(name='bintostr')
+async def bin_to_str(ctx, *, text):
+    encoded_text = bin_to_str(text)
+    await ctx.send(f"Original Text: {text}\nString: {encoded_text}")
+
+
+@bot.command(name='strtobin')
+async def str_to_str(ctx, *, text):
+    encoded_text = functions.str_to_bin(text)
+    await ctx.send(f"Original Text: {text}\nBinary Value: {encoded_text}")
+
+
+@bot.command(name="ctfs")
+async def get_ctf(ctx):
+    events = functions.get_ctf_events()
+    events_tostring = ""
+    if events:
+        for i in range(5):
+            dt_str = events[i]['start']
+            dt = datetime.fromisoformat(dt_str)
+            formatted_dt = dt.strftime("%Y-%m-%d - %H:%M")
+            events_tostring += f"{i+1}. "
+            events_tostring += f"**{events[i]['title']}**: "
+            events_tostring += formatted_dt
+            events_tostring += "\n"
+    await ctx.send("These are the 5 upcoming events: \n")
+    await ctx.send(events_tostring)
+
+
+@bot.command(name="team")
+async def get_ctf(ctx):
+    info = functions.team_info()
+    name = info["name"]
+    world_rating = info["rating"]["2024"]["rating_place"]
+    country_rating = info["rating"]["2024"]["country_place"]
+    country = info["country"]
+    info_tostring = f"Name: **{name}** \n World Rating: **{world_rating}** \n Country Rating: **{country_rating}** \n Country: **{country}**"
+    await ctx.send(info_tostring)
+
+@bot.command(name="jwtdec")
+async def jwt_dec(ctx, string: str, key: str):
+    info = functions.jwtdec(string, key)
+    await ctx.send(info)
+
+
+@bot.command(name="commands")
+async def help_command(ctx):
+    embed = discord.Embed(
+        title="Command List",
+        description="The list of the commands the bot can execute. The prefix is - '!'",
+        color=discord.Color.default()
+    )
+
+    embed.add_field(name='\u200b', value="ENCODERS", inline=False)
+    embed.add_field(name="64enc", value="Encoding a string using Base64", inline=False)
+    embed.add_field(name="32enc", value="Encoding a string using Base32", inline=False)
+    embed.add_field(name="urlenc", value="Encoding a URL", inline=False)
+
+    embed.add_field(name='\u200b', value="DECODERS", inline=False)
+    embed.add_field(name="64dec", value="Decoding a string that's encoded using Base64", inline=False)
+    embed.add_field(name="32dec", value="Decoding a string that's encoded using Base32", inline=False)
+    embed.add_field(name="urldec", value="Decoding a URL", inline=False)
+
+    embed.add_field(name='\u200b', value="GENERATORS", inline=False)
+    embed.add_field(name="cyc", value="Generating a cyclic pattern", inline=False)
+    embed.add_field(name="cycfind", value="Finding a subs in a cyclic pattern", inline=False)
+
+    embed.add_field(name='\u200b', value="Misc", inline=False)
+    embed.add_field(name="team", value="Printing the team's stats", inline=False)
+    embed.add_field(name="ctfs", value="Printing the 5 upcoming CTF contests", inline=False)
+
+    embed.set_footer(text="Luppole Made This")
+
+    await ctx.send(embed=embed)
+
+@bot.hybrid_command(name="ping", description="mmmmmmmm")
+async def ping(ctx):
+    await ctx.reply("Pong!")
+
+@bot.event
+async def on_ready():
+    await bot.tree.sync()
+    print(f'{bot.user} has connected to Discord!')
+
+# Run the bot with the token
+bot.run('MTE4ODU5Mzc5OTQxNzU3NzQ4Mg.GY2NVU.KDkz7zrTfshEy7wQizKp2Qv67rUTG7BbGfUuko')
